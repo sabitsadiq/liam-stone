650503164dfa90dd22bccde278c5a87f387987a9
sadiq <sabitsdq@gmail.com>
2023-06-24 01:31:02 +0100

    images correcion

--- components/P2pLogo.tsx
+++ components/P2pLogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const P2pLogo = () => {
   return (
     <div className="pt-8 w-11/12 pl-2 px-4 md:px-6 mx-auto">
-      <Image src="/image/p2p.png" width="100" height="100" alt="logo" />
+      <Image src="/image/logo2.png" width="100" height="100" alt="logo" />
     </div>
   );
 };
